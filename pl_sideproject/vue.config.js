07bd70a8946f44bd64ede4caf8e00542b7404ddc
jhpark2 <t2401043@partner.coway.com>
2024-06-28 10:14:58 +0900

    install vuetify

--- pl_sideproject/vue.config.js
+++ pl_sideproject/vue.config.js
@@ -10,10 +10,18 @@ module.exports = defineConfig({
       }),
     ],
   },
+
   transpileDependencies: true,
+
   devServer: {
     client: {
       overlay: false,
+    },
+  },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
 });
