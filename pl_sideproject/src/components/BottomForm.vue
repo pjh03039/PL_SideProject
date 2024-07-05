25c3d1005e1404810c3778a2eb521ac48b44717f
mhlee <mcnccoway4003@gmail.com>
2024-07-05 15:28:12 +0900

    바코드 생성 로딩2

--- pl_sideproject/src/components/BottomForm.vue
+++ pl_sideproject/src/components/BottomForm.vue
@@ -11,7 +11,22 @@
     <v-bottom-sheet v-model="sheet">
       <v-card height="500">
         <div class="px-3 py-3 text-end" @click="sheet = !sheet">
-          <v-icon icon="mdi-close" size="x-large"></v-icon>
+          <!-- <v-btn
+            :icon="`mdi-numeric-${bottomSheetCnt}`"
+            height="40"
+            variant="text"
+            width="40"
+            border
+          ></v-btn> -->
+          <v-progress-circular
+            :model-value="bottomSheetPer"
+            :rotate="200"
+            :size="20"
+            :width="4"
+            color="teal"
+          >
+            <span style="font-size: 7px">{{ bottomSheetCnt }}</span>
+          </v-progress-circular>
         </div>
         <v-card-text class="text-center">
           <div id="canvas-container"></div>
@@ -23,19 +38,18 @@
 
 <script setup>
 import html2canvas from 'html2canvas';
-import { ref, watch } from 'vue';
+import { computed, onBeforeUnmount, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
-const barcodeValue = store.getters.getBarcodeValue;
-const generateBarcode = () => {
-  console.log('바코드 :', barcodeValue);
-};
+
+// 바코드 복사
 
 function createBottomSheet() {
   setTimeout(() => {
     const container = document.getElementById('canvas-container');
     container.appendChild(canvas);
+    startInterval(1000);
   }, 100);
 }
 
@@ -77,6 +91,52 @@ async function copyQRCode() {
     console.error('요소 이미지 복사 중 오류 발생:', error);
   }
 }
+
+// 바텀 시트
+
+let bottomSheetMaxPer = 5;
+let bottomSheetCnt = ref(5);
+let bottomSheetPer = computed(() => {
+  return (bottomSheetCnt.value / bottomSheetMaxPer) * 100;
+});
+const barcodeValue = store.getters.getBarcodeValue;
+const generateBarcode = () => {
+  console.log('바코드 :', barcodeValue);
+};
+
+// 인터벌 ID를 저장할 변수
+let intervalId = null;
+
+// 인터벌을 시작하는 함수
+const startInterval = interval => {
+  bottomSheetCnt.value = 5;
+  // 기존 인터벌이 있다면 제거
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  // 새로운 인터벌 설정
+  intervalId = setInterval(() => {
+    bottomSheetCnt.value--;
+    console.log(bottomSheetCnt.value);
+    if (bottomSheetCnt.value <= 0) {
+      sheet.value = false;
+      stopInterval();
+    }
+  }, interval);
+};
+
+// 인터벌을 중지하는 함수
+const stopInterval = () => {
+  console.log('stopInterval');
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
+
+onBeforeUnmount(() => {
+  stopInterval();
+});
 </script>
 
 <style></style>
