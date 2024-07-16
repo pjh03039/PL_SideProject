1fcda725134f7b3350adeaa82d86c8d8a223cfac
jhpark2 <t2401043@partner.coway.com>
2024-07-16 10:51:52 +0900

    타이틀 변경 및 복사 시 버그 수정

--- pl_sideproject/src/components/barcodePage/ProductInfoForm.vue
+++ pl_sideproject/src/components/barcodePage/ProductInfoForm.vue
@@ -3,21 +3,19 @@
     <div class="text-center text-h5">
       <strong><span class="text-blue-darken-1">Coway</span> 제품입니다.</strong>
     </div>
-    <v-list>
-      <v-list-item>바코드 : {{ getProductObj.productBarcode }}</v-list-item>
-      <v-list-item
-        >자재코드 : {{ getProductObj.productInfo.makrModelCode }}</v-list-item
-      >
-      <v-list-item
-        >프로젝트명 : {{ getProductObj.productInfo.prodName }}</v-list-item
-      >
-      <v-list-item
-        >모델명 : {{ getProductObj.productInfo.productModel }}</v-list-item
-      >
-      <v-list-item
-        >제품 이름 : {{ getProductObj.productInfo.productName }}</v-list-item
-      >
-    </v-list>
+    <v-list-item>바코드 : {{ getProductObj.productBarcode }}</v-list-item>
+    <v-list-item
+      >자재코드 : {{ getProductObj.productInfo.makrModelCode }}</v-list-item
+    >
+    <v-list-item
+      >프로젝트명 : {{ getProductObj.productInfo.prodName }}</v-list-item
+    >
+    <v-list-item
+      >모델명 : {{ getProductObj.productInfo.productModel }}</v-list-item
+    >
+    <v-list-item
+      >제품 이름 : {{ getProductObj.productInfo.productName }}</v-list-item
+    >
   </v-container>
 </template>
 
